d9e37120dd2745869d896a45d05b47c8b559a99f
Marymezh <maria.mezhova@yahoo.com>
2021-09-21 17:24:21 +0700

    connected IB outlets and actions for the first VC + logics of the calculation

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        weightSlider.value = 100
+        heightSlider.value = 1.5
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let calculation = weight / pow(height, 2)
+        
+//        let secondVC = SecondViewController()
+//        secondVC.bmiValue = String (format: "%.0f", calculation)
+//        self.present(secondVC, animated: true, completion: nil)
+        
+    }
 }
 
